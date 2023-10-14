0713d2c8f77a842cac4feea58e021dfaf39d2317
Chaerim0626 <dasapcr@gmail.com>
2023-10-14 16:50:25 +0900

    :lipstick: 팀 소개 페이지 추가

--- components/layouts/Banner.js
+++ components/layouts/Banner.js
@@ -1,39 +1,18 @@
-import styled from '@emotion/styled'
-
-// 배너 이미지
-const BannerImg = styled.div`
-  height : 356px;
-  background-image : url('/images/banner.png');
-  background-size : cover;
-  background-position : center;
-  background-repeat : no-repeat;
-  position : relative;
-  margin-bottom : 50px;
-`;
-
-// 내부 Title
-const BannerTitle = styled.div`
-  width : 503px;
-  height : 178px;
-  border-radius : 20px 20px 0 0;
-  background : white;
-  position : absolute;
-  bottom : 0;
-  left : 0;
-  right : 0;
-  margin : 0 auto;
-  padding : 35px;
-  text-align : center;
-`;
+import Image from "next/image";
 
 // Banner
-export default function Banner(){
-  return(
-    <BannerImg>
-      <BannerTitle>
-        <h2 className="m-3 fs-1">{props.title}</h2>
-        <p className="fs-5">{props.subtitle}</p>
-      </BannerTitle>
-    </BannerImg>
-  )
-};
+function Banner(props) {
+  return (
+    <>
+      <div style={{ marginBottom: "20px" }}>
+        <Image src="/images/banner.png" alt="" width={2500} height={300} />
+      </div>
+      {/* <S.BannerTitle>
+        <h2>{props.title}</h2>
+        <p>{props.subtitle}</p>
+      </S.BannerTitle> */}
+    </>
+  );
+}
+
+export default Banner;
